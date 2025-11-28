e68b6942f915368851c294a5d938e16027d87f08
Azakana268 <167258062+Azakana268@users.noreply.github.com>
2025-11-28 10:42:48 -0300

    Add Tarotista model with user relationship

--- core/models.py
+++ core/models.py
@@ -24,3 +24,23 @@ class Reporte(models.Model):
     
     def __str__(self):
         return f"Reporte de {self.tarotista.usuario.get_full_name()} sobre {self.paciente.get_full_name()}"
+# En tu app de tarotistas (ej. tarotistas/models.py)
+
+from django.db import models
+from usuarios.models import Usuario # Importamos tu modelo de usuario personalizado
+
+class Tarotista(models.Model):
+    # La clave que conecta a la tabla de Usuarios (usuarios_usuario)
+    # primary_key=True es común para perfiles OneToOneField.
+    usuario = models.OneToOneField(
+        Usuario,  
+        on_delete=models.CASCADE,
+        related_name='tarotista' # Nombre de la relación inversa (request.user.tarotista)
+    )
+    
+    bio = models.TextField(blank=True, null=True, default="Tarotista en formación.")
+    especialidad = models.CharField(max_length=100, blank=True, null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Perfil de {self.usuario.get_full_name()}"
