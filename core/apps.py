81d7730c7f41879e0b25caee5ed6d0ee116c9f9c
Azakana268 <167258062+Azakana268@users.noreply.github.com>
2025-11-28 10:51:25 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+# En tu app de tarotistas (ej. tarotistas/apps.py)
+from django.apps import AppConfig
+
+class TarotistasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tarotistas'
+
+    # ESTA LÍNEA ES CLAVE: Importa el módulo de señales al iniciar la app.
+    def ready(self):
+        import tarotistas.signals
