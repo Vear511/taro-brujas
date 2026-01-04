d4a9989d45901e176a145d6b05d7dd77af27dce2
Azakana268 <167258062+Azakana268@users.noreply.github.com>
2026-01-04 18:10:43 -0300

    Update admin.py

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -5,31 +5,30 @@ from .models import Usuario
 
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
+    # Campos que existen en AbstractUser + tus extras actuales
     list_display = (
-        'username',
-        'email',
-        'first_name',
-        'last_name',
-        'is_staff',
-        'bloqueado',
-        'creado_en',
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "es_tarotista",
+        "email_verificado",
     )
 
     list_filter = (
-        'is_staff',
-        'is_superuser',
-        'bloqueado',
-        'creado_en',
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "es_tarotista",
+        "email_verificado",
     )
 
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+    # Agrega tus campos extra al formulario del admin
     fieldsets = UserAdmin.fieldsets + (
-        ('Información adicional', {
-            'fields': (
-                'telefono',
-                'fecha_nacimiento',
-                'bio',
-                'avatar',
-                'bloqueado',
-            )
-        }),
+        ("Campos extra", {"fields": ("avatar", "es_tarotista", "email_verificado")}),
     )
